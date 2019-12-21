a336129fd957ea0c4e6c84163baee4c083d38f05
Azhari Muhammad M <azhari.marzan@gmail.com>
2019-12-21 07:45:27 +0700

    + helper styling component

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import {View, Text, StyleSheet, Button} from 'react-native'
+import {View, StyleSheet} from 'react-native'
+import {Text, Input, Button} from 'react-native-elements'
+import Spacer from '../components/Spacer'
 
 const SignupScreen = ({navigation}) => {
     return (
         <>
-            <Text style={{fontSize: 48}}>SignupScreen</Text>
-            <Button title="Go to Signin" onPress={() => navigation.navigate('Signin')}></Button>
-            <Button title="Go to Main" onPress={() => navigation.navigate('mainFlow')}></Button>
+        <Spacer>
+            <Text h3>Sign Up for Tracker</Text>
+        </Spacer>
+            <Input label="Email" />
+            <Spacer />
+            <Input label="Password" />
+        <Spacer>
+            <Button title="Sign Up" />
+        </Spacer>
         </>
     )
 }
