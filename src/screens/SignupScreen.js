3e06dea338c001d4175f55c19636e198d56b6b78
Azhari Muhammad M <azhari.marzan@gmail.com>
2019-12-21 08:17:23 +0700

    ^ styling odds and ends

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -5,20 +5,32 @@ import Spacer from '../components/Spacer'
 
 const SignupScreen = ({navigation}) => {
     return (
-        <>
-        <Spacer>
-            <Text h3>Sign Up for Tracker</Text>
-        </Spacer>
-            <Input label="Email" />
-            <Spacer />
-            <Input label="Password" />
-        <Spacer>
-            <Button title="Sign Up" />
-        </Spacer>
-        </>
+        <View style={styles.container}>
+            <Spacer>
+                <Text h2>Sign Up for Tracker</Text>
+            </Spacer>
+                <Input label="Email" />
+                <Spacer />
+                <Input label="Password" />
+            <Spacer>
+                <Button title="Sign Up" />
+            </Spacer>
+        </View>
     )
 }
 
-const styles = StyleSheet.create({})
+SignupScreen.navigationOptions = () => {
+    return {
+        header: null
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 250
+    }
+})
 
 export default SignupScreen
