e1c767ae9c7f43e301552fe63f40fda048a76c56
Azhari Muhammad M <azhari.marzan@gmail.com>
2019-12-23 19:23:41 +0700

    + safeareaview

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -2,16 +2,19 @@ import React, {useContext} from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 import {Button} from 'react-native-elements'
 import Spacer from '../components/Spacer'
+import {SafeAreaView} from 'react-navigation'
 import {Context as AuthContext} from '../context/authContext'
 
 const AccountScreen = () => {
     const {signout} = useContext(AuthContext)
     return (
         <>
+        <SafeAreaView forceInset={{top: 'always'}}>
             <Text style={{fontSize: 48}}>AccountScreen</Text>
             <Spacer>
                 <Button title="Sign Out" onPress={signout}></Button>
             </Spacer>
+        </SafeAreaView>
         </>
     )
 }
