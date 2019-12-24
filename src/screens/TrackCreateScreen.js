95521033fbf7fa52b20a25bbb551055e6ff5c084
Azhari Muhammad M <azhari.marzan@gmail.com>
2019-12-24 18:57:53 +0700

    ^ update location context

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -9,9 +9,12 @@ import useLocation from '../hooks/useLocation'
 import TrackForm from '../components/TrackForm'
 
 const TrackCreateScreen = ({isFocused}) => {
-    const {addLocation} = useContext(LocationContext)
+    const {state, addLocation} = useContext(LocationContext)
 
-    const [err] = useLocation(isFocused, addLocation)
+    const [err] = useLocation(isFocused, (location) => {
+        addLocation(location, state.recording)
+    })
+    
     return (
         <SafeAreaView forceInset={{top: 'always'}}>
             <Text h2>Create a Track</Text>
