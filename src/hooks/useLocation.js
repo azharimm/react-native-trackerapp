983968a8e514a5bfefe9b12b9208a812c029130b
Azhari Muhammad M <azhari.marzan@gmail.com>
2019-12-24 15:40:31 +0700

    ^ automatic disable

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,26 +1,34 @@
 import {useState, useEffect} from 'react'
 import {Accuracy, requestPermissionsAsync, watchPositionAsync} from 'expo-location'
 
-export default (callback) => {
+export default (shouldTrack, callback) => {
     const [err, setErr] = useState(null)
+    const [subscriber, setSubscriber] = useState(null)
     const startWatching = async() => {
         try {
             await requestPermissionsAsync()
-            const subscriber = await watchPositionAsync({
+            const sub = await watchPositionAsync({
                 accuracy: Accuracy.BestForNavigation,
                 timeInterval: 1000,
                 distanceInterval: 10
             },
             callback
             )
+            setSubscriber(sub)
         } catch (error) {
             setErr(error)
         }
     }
 
     useEffect(() => {
-        startWatching()
-    }, [])
+        if(shouldTrack) {
+            startWatching()
+        }else {
+            //stop
+            subscriber.remove()
+            setSubscriber(null)
+        }
+    }, [shouldTrack])
 
     return [err]
 }
