57d92ea6ff4a3aa480a501c28ce60e5705c64204
Azhari Muhammad M <azhari.marzan@gmail.com>
2019-12-25 08:13:22 +0700

    ^ tracking while recording

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -3,29 +3,30 @@ import {Accuracy, requestPermissionsAsync, watchPositionAsync} from 'expo-locati
 
 export default (shouldTrack, callback) => {
     const [err, setErr] = useState(null)
-    const [subscriber, setSubscriber] = useState(null)
-    const startWatching = async() => {
-        try {
-            await requestPermissionsAsync()
-            const sub = await watchPositionAsync({
-                accuracy: Accuracy.BestForNavigation,
-                timeInterval: 1000,
-                distanceInterval: 10
-            },
-            callback
-            )
-            setSubscriber(sub)
-        } catch (error) {
-            setErr(error)
-        }
-    }
 
     useEffect(() => {
+        let subscriber
+        const startWatching = async() => {
+            try {
+                await requestPermissionsAsync()
+                subscriber = await watchPositionAsync({
+                    accuracy: Accuracy.BestForNavigation,
+                    timeInterval: 1000,
+                    distanceInterval: 10
+                },
+                callback
+                )
+            } catch (error) {
+                setErr(error)
+            }
+        }
+
         if(shouldTrack) {
             startWatching()
         }else {
-            //stop
-            subscriber.remove()
+            if(subscriber) {
+                subscriber.remove()
+            }
             setSubscriber(null)
         }
 
